5f3d79b8c8313f7d369041f44273f19426933b9f
teddy-clapp <teddy@quadrata.io>
2021-11-22 16:24:59 -0800

    deployed level locally

--- test/gamifications/NewLevel.spec.ts
+++ test/gamifications/NewLevel.spec.ts
@@ -4,22 +4,21 @@ import { MichelsonMap, TezosToolkit } from "@taquito/taquito"
 const scorerJsonCode = require('../../contracts/main/gamifications/Scorer.tz.json')
 const accounts = require('../../scripts/sandbox/accounts')
 
-describe("BuildLevel()", () => {
+describe("BuildLevel()", function() {
+    this.timeout(1000000)
+
     beforeEach(async () => {
         console.log("BuildLevel Test")
         const tezos = new TezosToolkit('http://localhost:8732');
         tezos.setProvider({ signer: await InMemorySigner.fromSecretKey(accounts.alice.sk) })
 
-        console.log(await tezos.tz.getBalance('tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb'))
-        const dataMap = new MichelsonMap();
-        dataMap.set('0', '0');
         const levelStorage = {
             trading_pair : accounts.alice.pkh, // contract of quipu contract
             score_token : accounts.alice.pkh,  // contract of score token
             score : 1,
             streak : 0,
             current_rank : 0,
-            possible_ranks : dataMap,
+            possible_ranks : new MichelsonMap(),
             multiplier : 1,
             owner : accounts.alice.pkh,
         }
@@ -38,5 +37,5 @@ describe("BuildLevel()", () => {
 
     it("correctly sets up storage", async () => {
 
-    });
-}) 
+    })
+})
